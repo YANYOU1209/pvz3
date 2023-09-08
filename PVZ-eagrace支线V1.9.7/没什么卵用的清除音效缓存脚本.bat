a439ca5a1bf7e6a3de533e43bc06dc68860315fb
YANYOU1209 <820172821@qq.com>
2023-09-08 12:50:09 +0800

    X01

---
+++ PVZ-eagrace支线V1.9.7/没什么卵用的清除音效缓存脚本.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+rd/s/q "C:\ProgramData\PopCap Games\PlantsVsZombies\cached\sounds"
+rd/s/q "D:\ProgramData\PopCap Games\PlantsVsZombies\cached\sounds"
+rd/s/q "E:\ProgramData\PopCap Games\PlantsVsZombies\cached\sounds"
+rd/s/q "F:\ProgramData\PopCap Games\PlantsVsZombies\cached\sounds"
